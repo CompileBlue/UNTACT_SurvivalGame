82947437eb77fab1a6633fdd68d60021257ec25b
ssw03270 <ttd8591@gmail.com>
2020-10-28 22:56:07 +0900

    [Add] - Add ItemControl.cs and ObjectControl.cs
    Add two script and there are imsi script.

---
+++ UNTACT/Assets/Scripts/IMSI/ObjectControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectControl : MonoBehaviour
+{
+    private bool isEnter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isEnter = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Interact();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        /* This conditional sentence is help you to interact with item or object.
+         * For example, when you stand in front of the  door, 
+         * If you press F button, the door is open.*/
+        if (collision.transform.tag == "Player")
+        {
+            Debug.Log("Interact with Player, Object");
+            isEnter = true;
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            Debug.Log("Away from Player, Object");
+            isEnter = false;
+        }
+    }
+    void Interact()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && isEnter)
+        {
+            Debug.Log("Press F Button");
+        }
+    }
+}
--- UNTACT/Assets/Scripts/PlayerControl.cs
+++ UNTACT/Assets/Scripts/PlayerControl.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed;
+
+    public Rigidbody2D playerRigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
@@ -19,21 +22,17 @@ public class PlayerControl : MonoBehaviour
     }
     void Move()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
-        }
+        /* This function is used for movement of player. 
+         * If you press Arrow Key, the player will move.*/
+
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * moveSpeed;
+        float ySpeed = yInput * moveSpeed;
+
+        Vector3 newVelocity = new Vector3(xSpeed, ySpeed, 0f);
+        playerRigidbody.velocity = newVelocity;
+        
     }
 }
