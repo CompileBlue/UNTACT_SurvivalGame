ba627996112905b7fcdd470183996e179b9856d3
JaeYoung Seon <42963882+tjswodud@users.noreply.github.com>
2020-11-06 00:58:12 +0900

    [Add] - add IntroManager.cs, TruckManager.cs, Truck.png
    IntroManager (Manage a 'Intro' Scene), TruckManager (Manage Truck's movement in intro scene)

---
+++ UNTACT/Assets/Scripts/IMSI/IntroManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue
+{
+    [TextArea]
+    public string txt;
+}
+
+public class IntroManager : MonoBehaviour
+{
+    public Image talkPanel;
+    public Text text;
+    public RawImage fadeImage;
+
+    private bool isTalk = false;
+    private int count = 0;
+    public float fadeSpeed = 0.4f;
+
+    public enum FadeDirection
+    {
+        In, // Alpha = 1
+        Out // Alpha = 0
+    }
+
+    [SerializeField] private Dialogue[] dialogue;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ShowDialogue();
+        }
+    }
+
+    public void ShowDialogue()
+    {
+        talkPanel.gameObject.SetActive(true);
+        text.gameObject.SetActive(true);
+    }
+
+    // Scene Fade-In
+    private void OnEnable()
+    {
+        StartCoroutine(Fade(FadeDirection.Out));
+    }
+
+    private IEnumerator Fade(FadeDirection fadeDirection)
+    {
+        float alpha = (fadeDirection == FadeDirection.Out) ? 1 : 0;
+        float fadeEndValue = (fadeDirection == FadeDirection.Out) ? 0 : 1;
+        if (fadeDirection == FadeDirection.Out)
+        {
+            while (alpha >= fadeEndValue)
+            {
+                SetColorImage(ref alpha, fadeDirection);
+                yield return null;
+            }
+            fadeImage.enabled = false;
+        }
+        else
+        {
+            fadeImage.enabled = true;
+            while (alpha <= fadeEndValue)
+            {
+                SetColorImage(ref alpha, fadeDirection);
+                yield return null;
+            }
+        }
+    }
+
+    private void SetColorImage(ref float alpha, FadeDirection fadeDirection)
+    {
+        fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, alpha);
+        alpha += Time.deltaTime * (1.0f / fadeSpeed) * ((fadeDirection == FadeDirection.Out) ? -1 : 1);
+    }
+}
---
+++ UNTACT/Assets/Scripts/IMSI/TruckManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TruckManager : MonoBehaviour
+{
+    public float moveSpeed;
+    public Transform endPoint;
+    public Transform startPoint;
+    public float delayTime = 1.2f;
+    private float fTickTime;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        if (Wait(delayTime))
+        {
+            Vector3 velocity = Vector3.zero;
+            transform.position = Vector3.MoveTowards(transform.position, endPoint.position, moveSpeed * Time.deltaTime);
+        }
+    }
+
+    public bool Wait(float delayTime)
+    {
+        fTickTime += Time.deltaTime;
+        return (fTickTime >= delayTime);
+    }
+}
