06d35ed47e313cb03fbd8098f7a3a84a495921fc
ssw03270 <ttd8591@gmail.com>
2020-11-06 00:04:30 +0900

    [Add] - Add PlayGround.unity and some CS file
    Add PlayGround.unity, LaptopControl.cs, RefrigeratorControl.cs, RoofControl.cs.

---
+++ UNTACT/Assets/Scripts/Interactive/RoofControl.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoofControl : MonoBehaviour
+{
+    SpriteRenderer sprender;
+
+    private bool isEnter;
+
+    private float transparency;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isEnter = false;
+        transparency = 1f;
+        sprender = gameObject.GetComponent<SpriteRenderer>();
+        sprender.color = new Color(1f, 1f, 1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invisible();
+    }
+    void Invisible()
+    {
+        if (isEnter)
+        {
+            if(transparency > 0)
+            {
+                transparency -= 0.02f;
+            }
+            else
+            {
+                transparency = 0;
+            }
+        }
+        else
+        {
+            if (transparency < 1)
+            {
+                transparency += 0.02f;
+            }
+            else
+            {
+                transparency = 1;
+            }
+        }
+        sprender.color = new Color(1f, 1f, 1f, transparency);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        /* This conditional sentence is help you to interact with item or object.
+         * For example, when you stand in front of the  door, 
+         * If you press F button, the door is open.*/
+        if (collision.transform.tag == "Player")
+        {
+            isEnter = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            isEnter = false;
+        }
+    }
+}
