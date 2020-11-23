41c7e218dc864de9eb64ea78828ae0dfdda3dc82
tjswodud <tjswodud85@gmail.com>
2020-11-23 22:31:23 +0900

    Revert "Merge branch 'JaeYoung' of https://github.com/CompileBlue/UNTACT_SurvivalGame into JaeYoung"
    This reverts commit ba3b157d17e776286cfb27caa3c3421e0fa04fb4, reversing
    changes made to fb491901d19d11a32eff4c992f3ebdd449a3102d.

--- UNTACT/Assets/Scripts/UI/LaptopControl.cs
+++ UNTACT/Assets/Scripts/UI/LaptopControl.cs
@@ -2,16 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public class LaptopControl : MonoBehaviour
 {
     public GameObject laptopUI;
-    public GameObject content;
-    public GameObject content_laptop;
-    public GameObject laptopPanelU;
-    public GameObject laptopPanelD;
-    public GameObject laptopPanelR;
 
     private bool isEnter;
     // Start is called before the first frame update
@@ -24,28 +18,6 @@ public class LaptopControl : MonoBehaviour
     void Update()
     {
         Interact();
-        foreach (var itemName in PlayerControl.inventoryList.Keys)
-        {
-            if (PlayerControl.inventoryList[itemName] >= 1)
-            {
-                Debug.Log(itemName);
-                if (!laptopPanelR.transform.FindChild(itemName))
-                {
-                    SetItemR_pc(itemName);
-                }
-            }
-        }
-        foreach (var itemName in PlayerControl.laptopList.Keys)
-        {
-            if (PlayerControl.laptopList[itemName] >= 1)
-            {
-                Debug.Log(itemName);
-                if (!laptopPanelD.transform.FindChild(itemName))
-                {
-                    SetItemD_pc(itemName);
-                }
-            }
-        }
     }
     void Interact()
     {
@@ -58,53 +30,6 @@ public class LaptopControl : MonoBehaviour
         {
             laptopUI.gameObject.SetActive(false);
         }
-    }
-
-    void SetItemR_pc(string itemName)
-    {
-        GameObject content_down = Instantiate(content);
-        content_down.transform.parent = laptopPanelR.transform;
-
-        content_down.transform.localScale = new Vector3(1f, 1f, 1f);
-        content_down.name = itemName;
-
-        GameObject icon = content_down.transform.GetChild(0).gameObject;
-        GameObject name = content_down.transform.GetChild(1).gameObject;
-        GameObject count = content_down.transform.GetChild(2).GetChild(0).gameObject;
-
-        string path = "Item/Icon/" + itemName;
-        Image icon_image = icon.GetComponent<Image>();
-        icon_image.sprite = Resources.Load<Sprite>(path);
-
-        TextMeshProUGUI name_text = name.GetComponent<TextMeshProUGUI>();
-        name_text.text = itemName;
-
-        TextMeshProUGUI count_text = count.GetComponent<TextMeshProUGUI>();
-        count_text.text = PlayerControl.inventoryList[itemName].ToString();
-    }
-
-    void SetItemD_pc(string itemName)
-    {
-        GameObject content_up = Instantiate(content_laptop);
-        content_up.transform.parent = laptopPanelD.transform;
-
-        content_up.transform.localScale = new Vector3(1f, 1f, 1f);
-        content_up.name = itemName;
-
-        GameObject icon = content_up;
-        GameObject count = content_up.transform.GetChild(0).gameObject;
-
-        string path = "Item/Icon/" + itemName;
-        Image icon_image = icon.GetComponent<Image>();
-        icon_image.sprite = Resources.Load<Sprite>(path);
-
-        TextMeshProUGUI count_text = count.GetComponent<TextMeshProUGUI>();
-        count_text.text = PlayerControl.laptopList[itemName].ToString();
-    }
-
-    void SetItemU_pc()
-    {
-        // item output
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
