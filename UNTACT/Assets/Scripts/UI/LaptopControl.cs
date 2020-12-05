1b707a46dfa586bf529824fd10935ba46dc1e77a
ssw03270 <ttd8591@gmail.com>
2020-12-05 23:10:34 +0900

    [Add] - Add laptop function

--- UNTACT/Assets/Scripts/UI/LaptopControl.cs
+++ UNTACT/Assets/Scripts/UI/LaptopControl.cs
@@ -7,6 +7,9 @@ using System.IO;
 
 public class LaptopControl : MonoBehaviour
 {
+    public GameObject screen;
+
+    private bool isEnter;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +19,28 @@ public class LaptopControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.F) && isEnter)
+        {
+            screen.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) || !isEnter)
+        {
+            screen.SetActive(false);
+        }
+    }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.name == "Refrigerator")
+        {
+            isEnter = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.name == "Refrigerator")
+        {
+            isEnter = false;
+        }
     }
 }
