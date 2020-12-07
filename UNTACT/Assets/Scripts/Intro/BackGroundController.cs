210332c7e25435673789c940a4dfe0b84cb715b2
tjswodud <tjswodud85@gmail.com>
2020-12-08 00:20:43 +0900

    [Add] add colliders

---
+++ UNTACT/Assets/Scripts/Intro/BackGroundController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundController : MonoBehaviour
+{
+    public float scrollSpeed = 0.5f;
+    Material bgMaterial;
+
+    void Start()
+    {
+        bgMaterial = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        float newOffsetX = bgMaterial.mainTextureOffset.x + scrollSpeed * Time.deltaTime;
+        Vector2 newOffset = new Vector2(newOffsetX, 0);
+
+        bgMaterial.mainTextureOffset = newOffset;
+    }
+}
